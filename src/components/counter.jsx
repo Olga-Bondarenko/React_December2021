7c40d31c4fe98cc59ba246d90af9574b3b74c399
Olga <olyapka2003@gmail.com>
2021-12-26 14:28:39 +0300

    conditional render

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const Counter = () => {
   const [count, setCount] = useState(1);
 
-  const tags = ["tag1", "tag2", "tag3"];
+  const tags = ["2"];
 
   const formCount = () => {
     return count === 0 ? "Ноль" : count;
@@ -13,12 +13,17 @@ const Counter = () => {
     classes += count === 0 ? "danger" : "primary";
     return classes;
   };
+  const renderTags = () => {
+    if (!tags.length) {
+      return "Тегов не найдено";
+    }
+    return tags.map((tag) => <li key={tag}>{tag}</li>);
+  };
 
   return (
     <>
-      {tags.map((tag) => (
-        <li key={tag}>{tag}</li>
-      ))}
+      {!tags.length && "Теги на найдены"}
+      {renderTags()}
       <span className={getBadgeClasses()}>{formCount()}</span>
       <button className="btn btn-secondary btn-sm">Increment </button>
     </>
