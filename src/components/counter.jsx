e48911608d5a01aa5687634c0ec78e299666bb4b
Olga <olyapka2003@gmail.com>
2021-12-26 14:02:46 +0300

    list render

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(1);
+
+  const tags = ["tag1", "tag2", "tag3"];
+
   const formCount = () => {
     return count === 0 ? "Ноль" : count;
   };
-  const getBadgeClesses = () => {
+  const getBadgeClasses = () => {
     let classes = "badge m-2 bg-";
     classes += count === 0 ? "danger" : "primary";
     return classes;
@@ -13,7 +16,10 @@ const Counter = () => {
 
   return (
     <>
-      <span className={getBadgeClesses()}>{formCount()}</span>
+      {tags.map((tag) => (
+        <li key={tag}>{tag}</li>
+      ))}
+      <span className={getBadgeClasses()}>{formCount()}</span>
       <button className="btn btn-secondary btn-sm">Increment </button>
     </>
   );
