df6f2cc1af0e5b8e10fe2036b2d78e9a374dc026
Olga <olyapka2003@gmail.com>
2022-01-02 22:31:02 +0300

    single source of truth

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,26 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 
 const Counter = (props) => {
   console.log(props);
-  const [value, setValue] = useState(props.value);
 
   const formValue = () => {
-    return value === 0 ? "Ноль" : value;
+    return props.value === 0 ? "Ноль" : props.value;
   };
   const getBadgeClasses = () => {
     let classes = "badge m-2 bg-";
-    classes += value === 0 ? "danger" : "primary";
+    classes += props.value === 0 ? "danger" : "primary";
     return classes;
   };
 
-  const handleIncrement = () => {
-    setValue(value + 1);
-  };
-
   const handleDecrement = () => {
-    if (value > 0) {
-      setValue(value - 1);
-    }
+    props.onDecrement(props.id, props.value);
   };
 
   return (
@@ -28,13 +21,13 @@ const Counter = (props) => {
       <h4>{props.name}</h4>
       <span className={getBadgeClasses()}>{formValue()}</span>
       <button
-        onClick={() => handleIncrement()}
+        onClick={() => props.onIncrement(props.id, props.value)}
         className="btn btn-secondary btn-sm m-1"
       >
         Increment
       </button>
       <button
-        onClick={() => handleDecrement()}
+        onClick={handleDecrement}
         className="btn btn-secondary btn-sm m-1"
       >
         Decrement
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,23 +2,49 @@ import React, { useState } from "react";
 import Counter from "./counter";
 
 const Counters = () => {
-  const [counters, setCounters] = useState([
+  const initialState = [
     { value: 0, id: 1, name: "Ложка" },
     { value: 0, id: 2, name: "Вилка" },
     { value: 0, id: 3, name: "Тарелка" },
     { value: 0, id: 4, name: "Кружка" },
     { value: 0, id: 5, name: "Что-то еще" },
-  ]);
+  ];
+  const [counters, setCounters] = useState(initialState);
 
   const handleDelete = (counterId) => {
-    console.log("handleDelete", counterId);
-    const newCounter = counters.filter((c) => c.id !== counterId);
-    setCounters(newCounter);
+    const newCounters = counters.filter((c) => c.id !== counterId);
+    setCounters(newCounters);
   };
+
+  const handleIncrement = (counterId, value) => {
+    const newCounters = [...counters];
+    const elementIndex = newCounters.findIndex((c) => c.id === counterId);
+    newCounters[elementIndex].value = value + 1;
+    setCounters(newCounters);
+  };
+
+  const handleDecrement = (counterId, value) => {
+    const newCounters = [...counters];
+    const elementIndex = newCounters.findIndex((c) => c.id === counterId);
+    newCounters[elementIndex].value = value - 1;
+    if (value > 0) setCounters(newCounters);
+  };
+
+  const handleReset = () => setCounters(initialState);
+
   return (
     <div>
+      <button onClick={handleReset} className="btn btn-primary btn-sm m-2">
+        Сброс
+      </button>
       {counters.map((counter) => (
-        <Counter key={counter.id} onDelete={handleDelete} {...counter} />
+        <Counter
+          key={counter.id}
+          onDelete={handleDelete}
+          onIncrement={handleIncrement}
+          onDecrement={handleDecrement}
+          {...counter}
+        />
       ))}
     </div>
   );
